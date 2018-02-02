87a7717e4cd9ea7c11cdf86c2b12f61dbcd57860
Hank <l945457758@gmail.com>
2018-02-02 11:07:03 +0800

    LoginServlet

--- src/servlet/LoginServlet.java
+++ src/servlet/LoginServlet.java
@@ -14,17 +14,6 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException{
-        request.setCharacterEncoding("utf-8");
-        response.setContentType("text/html");
 
-        User form = CommonUtils.toBean(request.getParameterMap(), User.class);
-        UserService userService = new UserService();
-        try{
-            User user = userService.login(form);
-            request.getSession().setAttribute("sessionUser", user);
-            response.sendRedirect(request.getContextPath() + "");
-        }catch(UserException e){
-
-        }
     }
 }
