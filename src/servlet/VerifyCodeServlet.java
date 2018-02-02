fc1c341665fb61e830b999f3ca8f0947b29736b6
Hank <l945457758@gmail.com>
2018-02-02 11:10:23 +0800

    VerifyCodeServlet

--- src/servlet/VerifyCodeServlet.java
+++ src/servlet/VerifyCodeServlet.java
@@ -1,12 +1,19 @@
 package servlet;
 
+import util.VerifyCode;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class VerifyCodeServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response){
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        VerifyCode verifyCode = new VerifyCode();
+        BufferedImage bufferedImage = verifyCode.getImage();
+        request.getSession().setAttribute("session_vcode", verifyCode.getText());
+        VerifyCode.output(bufferedImage, response.getOutputStream());
     }
 }
