40e6b0cfa32930cc160e11d620e5ed6ce89ecf8b
Hank <l945457758@gmail.com>
2018-02-28 22:26:44 +0800

    Update UserDao

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -1,59 +1,43 @@
 package dao;
 
-import cn.itcast.jdbc.TxQueryRunner;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import domain.User;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.ScalarHandler;
-
+import org.apache.commons.dbutils.handlers.BeanHandler;
 import java.sql.SQLException;
 
 public class UserDao {
-    private QueryRunner queryRunner = new TxQueryRunner();
 
+    //配置数据源
+    private ComboPooledDataSource dataSource = new ComboPooledDataSource();
+    //将数据源作为QueryRunner的参数
+    private QueryRunner queryRunner = new QueryRunner(dataSource);
+
+    //添加用户
     public void add(User user){
         try{
-            String sql = "INSERT INTO user VALUES (?,?,?,?,?)";
-            Object[] parameter = {user.getID(), user.getPassword(), user.getPassword(),
+            //sql语句中的 ？将用Object数组元素作为替换参数
+            String sql = "INSERT INTO user VALUES (?,?,?,?)";
+            Object[] parameter = {user.getUsername(), user.getPassword(),
                     user.getPhone(), user.getEmail()};
-
+            //执行sql语句
             queryRunner.update(sql,parameter);
         }catch (SQLException e){
             e.printStackTrace();
         }
     }
 
-    /*public void edit(User user){
-        try{
-            String sql = "UPDATE user SET username = ?, password = ?, phone = ?," +
-                        " email = ?";
-            Object[] parameter = {user.getPassword(), user.getPassword(),
-                                    user.getPhone(), user.getEmail()};
-            queryRunner.update(sql, parameter);
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }
-
-    public void delete(String id){
-        try{
-            String sql = "DELETE FROM user WHERE id = ?";
-            queryRunner.update(sql, id);
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-    }*/
-
+    //在注册和登陆之前，都需要在数据库中查找用户
     public User findUser(String username){
+        //实例化
         User user = new User();
         try{
+            //用username作为参数，将查询结果封装并返回
             String sql = "SELECT * FROM user WHERE username = ?";
-            queryRunner.query(sql, new ScalarHandler<>(),username);
-            user.setUsername(username);
+            user = queryRunner.query(sql, new BeanHandler<>(User.class), username);
         }catch (SQLException e){
             e.printStackTrace();
         }
         return user;
     }
-
-
 }
